fc26c4e4d7f817ba986ab212c8480d1381671397
Russell Propert <russellpropert@gmail.com>
2024-08-16 23:17:17 -0400

    Adding touch event listners.

--- touchcoordinates.js
+++ touchcoordinates.js
@@ -1,31 +1,116 @@
-const version = '1.2';
+const version = '1.21';
 
 let touchCoordinates0;
 
 const topContainer = document.getElementById('top-container');
 
 const displayVersion = document.getElementById('displayVersion');
-const displayXCoordinate0 = document.getElementById('displayXCoordinate0');
-const displayYCoordinate0 = document.getElementById('displayYCoordinate0');
-const touch0 = document.getElementById('touch0');
+const displayClientX = document.getElementById('displayClientX');
+const displayClientY = document.getElementById('displayClientY');
+const displayPageX = document.getElementById('displayPageX');
+const displayPageY = document.getElementById('displayPageY');
+const displayScreenX = document.getElementById('displayScreenX');
+const displayScreenY = document.getElementById('displayScreenY');
+const displayRadiusX = document.getElementById('displayRadiusX');
+const displayRadiusY = document.getElementById('displayRadiusY');
+const displayRotationAngle = document.getElementById('displayRotationAngle');
 
-document.ontouchmove = (event) => {
-  touchCoordinates0 = event.changedTouches.item(0);
+displayClientX.textContent = 'clientX: undefined';
+displayClientY.textContent = 'clientY: undefined';
+displayPageX.textContent = 'pageX: undefined'
+displayPageY.textContent = 'pageY: undefined'
+displayScreenX.textContent = 'screenX: undefined';
+displayScreenY.textContent = 'screenY: undefined';
+displayRadiusX.textContent = 'radiusX: undefined';
+displayRadiusY.textContent = 'radiusY: undefined';
+displayRotationAngle.textContent = 'rotationAngle: undefined';
+
+document.addEventListener('mousemove', event => {
+  console.log('Mouse Move');
+});
+
+document.addEventListener('touchstart', event => {
   event.preventDefault();
-  displayXCoordinate0.textContent = `X0: ${touchCoordinates0.pageX}`;
-  displayYCoordinate0.textContent = `Y0: ${touchCoordinates0.pageY}`;
-  touch0.style.width = `${touchCoordinates0.radiusX * 2}px`;
-  touch0.style.height = `${touchCoordinates0.radiusY * 2}px`;
-  touch0.style.transform = `${touchCoordinates0.rotationAngle}deg`;
-}
+  //console.log(event);
+  displayClientX.textContent = `clientX: ${Math.round(event.changedTouches[0].clientX)}`;
+  displayClientY.textContent = `clientY: ${Math.round(event.changedTouches[0].clientY)}`;
+  displayPageX.textContent = `pageX: ${Math.round(event.changedTouches[0].pageX)}`;
+  displayPageY.textContent = `pageY: ${Math.round(event.changedTouches[0].pageY)}`;
+  displayScreenX.textContent = `screenX: ${Math.round(event.changedTouches[0].screenX)}`;
+  displayScreenY.textContent = `screenY: ${Math.round(event.changedTouches[0].screenY)}`;
+  displayRadiusX.textContent = `radiusX: ${Math.round(event.changedTouches[0].radiusX)}`;
+  displayRadiusY.textContent = `radiusY: ${Math.round(event.changedTouches[0].radiusY)}`;
+  displayRotationAngle.textContent = `rotationAngle: ${Math.round(event.changedTouches[0].rotationAngle)}`;
 
-// document.addEventListener('touchstart', handleTouchEvent);
-// document.addEventListener('touchmove', handleTouchEvent);
-// document.addEventListener('touchend', handleTouchEvent);
+  for (let i = 0; i < event.changedTouches.length; i++) {
+    let touchDiv = document.createElement('div');
+    touchDiv.id = event.changedTouches[i].identifier;
+    touchDiv.className = 'touchDiv';
+    topContainer.appendChild(touchDiv);
+    touchDiv.style.top = `${event.changedTouches[i].pageY}px`;
+    touchDiv.style.left = `${event.changedTouches[i].pageX}px`;
+  }
+}, { passive: false }); 
 
-document.onmousemove = (event) => {
-    displayXCoordinate0.textContent = `X0: ${event.pageX}`;
-    displayYCoordinate0.textContent = `Y0: ${event.pageY}`;  
-};
+document.addEventListener('touchmove', event => {
+  displayClientX.textContent = `clientX: ${Math.round(event.changedTouches[0].clientX)}`;
+  displayClientY.textContent = `clientY: ${Math.round(event.changedTouches[0].clientY)}`;
+  displayPageX.textContent = `pageX: ${Math.round(event.changedTouches[0].pageX)}`;
+  displayPageY.textContent = `pageY: ${Math.round(event.changedTouches[0].pageY)}`;
+  displayScreenX.textContent = `screenX: ${Math.round(event.changedTouches[0].screenX)}`;
+  displayScreenY.textContent = `screenY: ${Math.round(event.changedTouches[0].screenY)}`;
+  displayRadiusX.textContent = `radiusX: ${Math.round(event.changedTouches[0].radiusX)}`;
+  displayRadiusY.textContent = `radiusY: ${Math.round(event.changedTouches[0].radiusY)}`;
+  displayRotationAngle.textContent = `rotationAngle: ${Math.round(event.changedTouches[0].rotationAngle)}`;
+
+  for (let i = 0; i < event.changedTouches.length; i++) {
+    let touchDiv = document.getElementById(event.changedTouches[i].identifier);
+    touchDiv.style.top = `${event.changedTouches[i].pageY}px`;
+    touchDiv.style.left = `${event.changedTouches[i].pageX}px`;
+  }
+});
+
+document.addEventListener('touchend', event =>{
+  displayClientX.textContent = `clientX: ${Math.round(event.changedTouches[0].clientX)}`;
+  displayClientY.textContent = `clientY: ${Math.round(event.changedTouches[0].clientY)}`;
+  displayPageX.textContent = `pageX: ${Math.round(event.changedTouches[0].pageX)}`;
+  displayPageY.textContent = `pageY: ${Math.round(event.changedTouches[0].pageY)}`;
+  displayScreenX.textContent = `screenX: ${Math.round(event.changedTouches[0].screenX)}`;
+  displayScreenY.textContent = `screenY: ${Math.round(event.changedTouches[0].screenY)}`;
+  displayRadiusX.textContent = `radiusX: ${Math.round(event.changedTouches[0].radiusX)}`;
+  displayRadiusY.textContent = `radiusY: ${Math.round(event.changedTouches[0].radiusY)}`;
+  displayRotationAngle.textContent = `rotationAngle: ${Math.round(event.changedTouches[0].rotationAngle)}`;
+
+  for (let i = 0; i < event.changedTouches.length; i++) {
+    let touchDiv = document.getElementById(event.changedTouches[i].identifier);
+    touchDiv.remove();
+  }
+});
+
+document.addEventListener('touchcancel', event =>{
+  console.log('Touch Cancel');
+  for (let i = 0; i < event.changedTouches.length; i++) {
+    let touchDiv = document.getElementById(event.changedTouches[i].identifier);
+    touchDiv.remove();
+  }
+});
+
+
+
+// document.ontouchmove = (event) => {
+//   touchCoordinates0 = event.changedTouches.item(0);
+//   event.preventDefault();
+//   displayXCoordinate0.textContent = `X0: ${touchCoordinates0.pageX}`;
+//   displayYCoordinate0.textContent = `Y0: ${touchCoordinates0.pageY}`;
+//   touch0.style.width = `${touchCoordinates0.radiusX * 2}px`;
+//   touch0.style.height = `${touchCoordinates0.radiusY * 2}px`;
+//   touch0.style.transform = `${touchCoordinates0.rotationAngle}deg`;
+// }
+
+
+// document.onmousemove = (event) => {
+//     displayXCoordinate0.textContent = `X0: ${event.pageX}`;
+//     displayYCoordinate0.textContent = `Y0: ${event.pageY}`;  
+// };
 
 displayVersion.textContent = `Version: ${version}`;
