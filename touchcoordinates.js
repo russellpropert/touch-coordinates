812e9e2064577d87352ccca67bce985097d3e304
Russell Propert <russellpropert@gmail.com>
2024-08-20 20:10:00 -0400

    Adding ID over touch points and better handling for removing touch points.

--- touchcoordinates.js
+++ touchcoordinates.js
@@ -1,116 +1,127 @@
-const version = '1.21';
-
-let touchCoordinates0;
-
 const topContainer = document.getElementById('top-container');
 
-const displayVersion = document.getElementById('displayVersion');
-const displayClientX = document.getElementById('displayClientX');
-const displayClientY = document.getElementById('displayClientY');
 const displayPageX = document.getElementById('displayPageX');
 const displayPageY = document.getElementById('displayPageY');
-const displayScreenX = document.getElementById('displayScreenX');
-const displayScreenY = document.getElementById('displayScreenY');
-const displayRadiusX = document.getElementById('displayRadiusX');
-const displayRadiusY = document.getElementById('displayRadiusY');
-const displayRotationAngle = document.getElementById('displayRotationAngle');
-
-displayClientX.textContent = 'clientX: undefined';
-displayClientY.textContent = 'clientY: undefined';
-displayPageX.textContent = 'pageX: undefined'
-displayPageY.textContent = 'pageY: undefined'
-displayScreenX.textContent = 'screenX: undefined';
-displayScreenY.textContent = 'screenY: undefined';
-displayRadiusX.textContent = 'radiusX: undefined';
-displayRadiusY.textContent = 'radiusY: undefined';
-displayRotationAngle.textContent = 'rotationAngle: undefined';
-
-document.addEventListener('mousemove', event => {
-  console.log('Mouse Move');
-});
 
 document.addEventListener('touchstart', event => {
   event.preventDefault();
-  //console.log(event);
-  displayClientX.textContent = `clientX: ${Math.round(event.changedTouches[0].clientX)}`;
-  displayClientY.textContent = `clientY: ${Math.round(event.changedTouches[0].clientY)}`;
-  displayPageX.textContent = `pageX: ${Math.round(event.changedTouches[0].pageX)}`;
-  displayPageY.textContent = `pageY: ${Math.round(event.changedTouches[0].pageY)}`;
-  displayScreenX.textContent = `screenX: ${Math.round(event.changedTouches[0].screenX)}`;
-  displayScreenY.textContent = `screenY: ${Math.round(event.changedTouches[0].screenY)}`;
-  displayRadiusX.textContent = `radiusX: ${Math.round(event.changedTouches[0].radiusX)}`;
-  displayRadiusY.textContent = `radiusY: ${Math.round(event.changedTouches[0].radiusY)}`;
-  displayRotationAngle.textContent = `rotationAngle: ${Math.round(event.changedTouches[0].rotationAngle)}`;
 
   for (let i = 0; i < event.changedTouches.length; i++) {
-    let touchDiv = document.createElement('div');
-    touchDiv.id = event.changedTouches[i].identifier;
-    touchDiv.className = 'touchDiv';
-    topContainer.appendChild(touchDiv);
-    touchDiv.style.top = `${event.changedTouches[i].pageY}px`;
-    touchDiv.style.left = `${event.changedTouches[i].pageX}px`;
+    let id = event.changedTouches[i].identifier;
+    let x = Math.round(event.changedTouches[i].pageX);
+    let y = Math.round(event.changedTouches[i].pageY);
+
+    let dataBox = document.createElement('div');
+    dataBox.id = `dataBoxFor${id}`;
+    dataBox.className = 'dataBox';
+    dataBox.textContent = `Touch ID: ${id}`;
+    topContainer.appendChild(dataBox);
+
+    let displayPageX = document.createElement('div');
+    let displayPageY = document.createElement('div');
+    displayPageX.id = `displayPageXFor${id}`;
+    displayPageY.id = `displayPageYFor${id}`;
+    displayPageX.textContent = `pageX: ${x}`;
+    displayPageY.textContent = `pageY: ${y}`;
+    dataBox.appendChild(displayPageX);
+    dataBox.appendChild(displayPageY);
+
+    let touch = document.createElement('div');
+    touch.id = id;
+    touch.className = 'touch';
+    topContainer.appendChild(touch);
+    touch.style.top = `${y}px`;
+    touch.style.left = `${x}px`;
+
+    let touchID = document.createElement('div');
+    touchID.className = 'touchID';
+    touchID.textContent = id;
+    touch.appendChild(touchID);
+
+    let touchPoint = document.createElement('div');
+    touchPoint.className = 'touchPoint';
+    touch.appendChild(touchPoint);
   }
 }, { passive: false }); 
 
 document.addEventListener('touchmove', event => {
-  displayClientX.textContent = `clientX: ${Math.round(event.changedTouches[0].clientX)}`;
-  displayClientY.textContent = `clientY: ${Math.round(event.changedTouches[0].clientY)}`;
-  displayPageX.textContent = `pageX: ${Math.round(event.changedTouches[0].pageX)}`;
-  displayPageY.textContent = `pageY: ${Math.round(event.changedTouches[0].pageY)}`;
-  displayScreenX.textContent = `screenX: ${Math.round(event.changedTouches[0].screenX)}`;
-  displayScreenY.textContent = `screenY: ${Math.round(event.changedTouches[0].screenY)}`;
-  displayRadiusX.textContent = `radiusX: ${Math.round(event.changedTouches[0].radiusX)}`;
-  displayRadiusY.textContent = `radiusY: ${Math.round(event.changedTouches[0].radiusY)}`;
-  displayRotationAngle.textContent = `rotationAngle: ${Math.round(event.changedTouches[0].rotationAngle)}`;
-
   for (let i = 0; i < event.changedTouches.length; i++) {
-    let touchDiv = document.getElementById(event.changedTouches[i].identifier);
-    touchDiv.style.top = `${event.changedTouches[i].pageY}px`;
-    touchDiv.style.left = `${event.changedTouches[i].pageX}px`;
+    let id = event.changedTouches[i].identifier;
+    let x = Math.round(event.changedTouches[i].pageX);
+    let y = Math.round(event.changedTouches[i].pageY);
+
+    let displayPageX = document.getElementById(`displayPageXFor${id}`);
+    let displayPageY = document.getElementById(`displayPageYFor${id}`);
+    displayPageX.textContent = `pageX: ${x}`;
+    displayPageY.textContent = `pageY: ${y}`;
+
+    let touch = document.getElementById(id);
+    touch.style.top = `${y}px`;
+    touch.style.left = `${x}px`;
   }
 });
 
 document.addEventListener('touchend', event =>{
-  displayClientX.textContent = `clientX: ${Math.round(event.changedTouches[0].clientX)}`;
-  displayClientY.textContent = `clientY: ${Math.round(event.changedTouches[0].clientY)}`;
-  displayPageX.textContent = `pageX: ${Math.round(event.changedTouches[0].pageX)}`;
-  displayPageY.textContent = `pageY: ${Math.round(event.changedTouches[0].pageY)}`;
-  displayScreenX.textContent = `screenX: ${Math.round(event.changedTouches[0].screenX)}`;
-  displayScreenY.textContent = `screenY: ${Math.round(event.changedTouches[0].screenY)}`;
-  displayRadiusX.textContent = `radiusX: ${Math.round(event.changedTouches[0].radiusX)}`;
-  displayRadiusY.textContent = `radiusY: ${Math.round(event.changedTouches[0].radiusY)}`;
-  displayRotationAngle.textContent = `rotationAngle: ${Math.round(event.changedTouches[0].rotationAngle)}`;
-
   for (let i = 0; i < event.changedTouches.length; i++) {
-    let touchDiv = document.getElementById(event.changedTouches[i].identifier);
-    touchDiv.remove();
+    let divNodeList = document.querySelectorAll('div.touch');
+    let divIDList = [];
+    divNodeList.forEach(node => {
+      divIDList.push(node.id);
+    });
+
+    let actualTouches = event.touches;
+    let actualTouchesList = []
+    for (let i = 0; i < actualTouches.length; i++) {
+      actualTouchesList.push(String(actualTouches[i].identifier));
+    }
+
+    divIDList.forEach(id => {
+      if (!actualTouchesList.includes(id)) {
+        let dataBoxToRemove = document.getElementById(`dataBoxFor${id}`);
+        let touchToRemove = document.getElementById(id);
+        dataBoxToRemove.remove();
+        touchToRemove.remove();
+      }
+    });
+
+    // Alternative way of handling this which did not seem as robust.
+    // let dataBox = document.getElementById(`dataBoxFor${event.changedTouches[i].identifier}`);
+    // dataBox.remove();
+
+    // let touch = document.getElementById(event.changedTouches[i].identifier);
+    // touch.remove();
   }
 });
 
 document.addEventListener('touchcancel', event =>{
   console.log('Touch Cancel');
   for (let i = 0; i < event.changedTouches.length; i++) {
-    let touchDiv = document.getElementById(event.changedTouches[i].identifier);
-    touchDiv.remove();
+    let divNodeList = document.querySelectorAll('div.touch');
+    let divIDList = [];
+    divNodeList.forEach(node => {
+      divIDList.push(node.id);
+    });
+
+    let actualTouches = event.touches;
+    let actualTouchesList = []
+    for (let i = 0; i < actualTouches.length; i++) {
+      actualTouchesList.push(String(actualTouches[i].identifier));
+    }
+
+    divIDList.forEach(id => {
+      if (!actualTouchesList.includes(id)) {
+        let dataBoxToRemove = document.getElementById(`dataBoxFor${id}`);
+        let touchToRemove = document.getElementById(id);
+        dataBoxToRemove.remove();
+        touchToRemove.remove();
+      }
+    });
+
+    // Alternative way of handling this which did not seem as robust.
+    // let dataBox = document.getElementById(`dataBoxFor${event.changedTouches[i].identifier}`);
+    // dataBox.remove();
+
+    // let touch = document.getElementById(event.changedTouches[i].identifier);
+    // touch.remove();
   }
 });
-
-
-
-// document.ontouchmove = (event) => {
-//   touchCoordinates0 = event.changedTouches.item(0);
-//   event.preventDefault();
-//   displayXCoordinate0.textContent = `X0: ${touchCoordinates0.pageX}`;
-//   displayYCoordinate0.textContent = `Y0: ${touchCoordinates0.pageY}`;
-//   touch0.style.width = `${touchCoordinates0.radiusX * 2}px`;
-//   touch0.style.height = `${touchCoordinates0.radiusY * 2}px`;
-//   touch0.style.transform = `${touchCoordinates0.rotationAngle}deg`;
-// }
-
-
-// document.onmousemove = (event) => {
-//     displayXCoordinate0.textContent = `X0: ${event.pageX}`;
-//     displayYCoordinate0.textContent = `Y0: ${event.pageY}`;  
-// };
-
-displayVersion.textContent = `Version: ${version}`;
